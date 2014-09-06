53a44487350a53ad46165c7978da534ebdbaf897
Garrett Fidalgo <garrettfid@gmail.com>
2014-09-06 15:18:27 -0700

    Save Evernote token on callback

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,10 +1,10 @@
 class CallbacksController < ApplicationController
+  before_filter :authenticate
   # Evernote callback
   def evernote
     access_token = session[ :evernote_request_token ].get_access_token(oauth_verifier: params[:oauth_verifier])
     token = access_token.token
     session[ :evernote_access_token ] = token
-    client = EvernoteClient.new token: token
-    @notebooks = client.note_store.listNotebooks token
+    EvernoteAccessToken.create! token: token, account: current_account
   end
 end
