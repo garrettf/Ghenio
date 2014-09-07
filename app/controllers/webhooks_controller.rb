bc94397ab46c2f5a5c63e946ffba61995b1d5d9e
Garrett Fidalgo <garrettfid@gmail.com>
2014-09-06 21:20:42 -0700

    stop CSRF in webhooks

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,5 +1,7 @@
 class WebhooksController < ApplicationController
 
+  skip_before_filter :verify_authenticity_token
+
   def evernote
     head :ok unless params[ :userId ]
     evernote_client = get_evernote_client params[:userId]
