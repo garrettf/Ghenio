8db3ecbc4ac0c23e861effac20bcdf1520104141
Garrett Fidalgo <garrettfid@gmail.com>
2014-09-06 20:09:50 -0700

    match correct notebook when signup

--- app/controllers/synchronizations_controller.rb
+++ app/controllers/synchronizations_controller.rb
@@ -5,7 +5,7 @@ class SynchronizationsController < ApplicationController
     repo = Repo.create! name: params[ :repo_name ]
     current_account.evernote_client
     guid = current_account.evernote_client.note_store.listNotebooks.detect do |nb|
-      nb.name == 'First Notebook'
+      nb.name == params[ :notebook_name ]
     end.guid
     notebook = Notebook.create! name: params[ :notebook_name ], evernote_guid: guid
     Synchronization.create! repo: repo, notebook: notebook, account: current_account
