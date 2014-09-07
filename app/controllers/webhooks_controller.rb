cbb7eb83837305c179207314fd4adc2a82ec41e3
Garrett Fidalgo <garrettfid@gmail.com>
2014-09-06 21:16:31 -0700

    Github webhook

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -20,10 +20,40 @@ class WebhooksController < ApplicationController
     head :ok
   end
 
+  def github
+    json = ActiveSupport::JSON.decode(request.body)
+    repo_string = json['repository']['full_name']
+    repo = Repo.find_by_name(repo_str)
+    synchro = repo.synchronization
+    en_client = synchro.account.evernote_client
+    en_user_id = en_client.user_store.getUser.id
+    client = octokit_client_for_repo repo_string
+    mod_arys = json['commits'].map do |com|
+      com[ 'modified' ]
+    end
+    mod_arys.each do |modified|
+      modified.each do |filename|
+        file = client.content repo_string, path: filename
+        text = Base64.decode(file.content)
+        note_name = filename.chomp( '.html' )
+        committer = EvernoteCommitter.new( synchro.notebook.name )
+        committer.update_note(
+          note_name: note_name,
+          note_contents: text,
+          evernoteUserId: en_user_id
+        )
+      end
+    end
+  end
+
   private
 
   def access_token_string user_id
     EvernoteAccessToken.find_by_evernote_user_id(user_id).token
+  end
+
+  def octokit_client_for_repo repo_str
+    Repo.find_by_name(repo_str).synchronization.account.octokit_client
   end
 
   def get_evernote_client(userId)
