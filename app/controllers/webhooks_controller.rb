69a7d69fe7662298b9edece1c0469383afec4554
Garrett Fidalgo <garrettfid@gmail.com>
2014-09-06 19:52:57 -0700

    paymon 2

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -4,7 +4,7 @@ class WebhooksController < ApplicationController
     evernote_client = get_evernote_client params[:userId]
 
     if params[:reason] == "create" || params[:reason] == "update"
-      note = get_note evernote_client, params[:guid]
+      note = get_note evernote_client, params[:guid], params[ :userId ]
       notebook = Notebook.find_by_evernote_guid(params[:notebookGuid])
       repo = Synchronization.find_by_notebook_id(notebook.evernote_guid).repo
 
@@ -15,13 +15,19 @@ class WebhooksController < ApplicationController
     head :ok
   end
 
+  private
+
+  def access_token_string user_id
+    EvernoteAccessToken.find_by_evernote_user_id(user_id).token
+  end
+
   def get_evernote_client(userId)
     @evernote_client ||= EvernoteClient.new(
-      token: EvernoteAccessToken.find_by_evernote_user_id(userId).token
+      token: access_token_string(userID)
     )
   end
 
-  def get_note(evernote_client, noteId)
-    evernote_client.note_store.getNote( evernote_client.token, noteId, true, false, false, false )
+  def get_note(evernote_client, noteId, user_id)
+    evernote_client.note_store.getNote( access_token_string( user_id ), noteId, true, false, false, false )
   end
 end
